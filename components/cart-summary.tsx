6366dfeabe1c06efccbb2c628861f321bdad38ea
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-22 09:48:32 -0500

    update

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -8,6 +8,8 @@ import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
   const { formattedTotalPrice, totalPrice, cartDetails, cartCount } = useShoppingCart()
+  const [isLoading, setLoading] = useState(false)
+  const isDisabled = isLoading || cartCount! === 0
   const shippingAmount = cartCount! > 0 ? 500 : 0
   const totalAmount = totalPrice! + shippingAmount
 
@@ -40,9 +42,9 @@ export function CartSummary() {
       </dl>
 
       <div className="mt-6">
-        <Button className="w-full">
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          Loading...
+        <Button  onClick={onCheckout} className="w-full" disabled={isDisabled} >
+         { isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+         { isLoading ?  "Loading..." : "Checkout" }
         </Button>
       </div>
     </section>
