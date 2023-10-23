9e1ce0be3d68133586aec6dcda979df01faab4a8
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-23 09:48:41 -0500

    update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "skAtTyC1BjlmsO6S5yBjTWNMXrrNnGhOmvcxv3f2Xp4orz6y7llLqcmf4RrS06a5oQD3T0ReSOHOKrruZMmWBqZGKRxGcSWrvM18Jb5pnqgsaYlRwejuSnBTVvhJK9X6NU1ZEyWfOlFfHf5WvTqaVDf57UQoeNGnJjzrBgs98D6sQynWqPSQ"
+  // token: "skAtTyC1BjlmsO6S5yBjTWNMXrrNnGhOmvcxv3f2Xp4orz6y7llLqcmf4RrS06a5oQD3T0ReSOHOKrruZMmWBqZGKRxGcSWrvM18Jb5pnqgsaYlRwejuSnBTVvhJK9X6NU1ZEyWfOlFfHf5WvTqaVDf57UQoeNGnJjzrBgs98D6sQynWqPSQ"
 })
